6c3704499c5875bd43e147bfb8f7c0add60dabbc
guestt <michalsmoladev@gmail.com>
2023-05-05 20:57:33 +0200

    add roles, uppercase sql

--- db.sql
+++ db.sql
@@ -3,11 +3,12 @@ CREATE DATABASE terrapp;
 USE terrapp;
 
 CREATE TABLE user (
-                      id int not null auto_increment primary key,
-                      email varchar(120) not null,
-                      password varchar(255) not null,
-                      created_at datetime not null,
-                      updated_at datetime
+                      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+                      email VARCHAR(120) NOT NULL,
+                      password VARCHAR(255) NOT NULL,
+                      roles JSON NOT NULL,
+                      created_at DATETIME NOT NULL,
+                      updated_at DATETIME
 );
 
 INSERT INTO user (email, password, created_at) VALUES ('testowy@gmail.com', '$2y$13$2fF1fTH6Psy/9ToPypADF.FrVdATVUrx/HHiBS7Uj4YVp0t7/E0Vy', NOW());
