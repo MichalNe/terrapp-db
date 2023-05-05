0d40c11ad66aef6dc696a31b10c00244dc776b03
guestt <michalsmoladev@gmail.com>
2023-05-05 21:00:21 +0200

    add roles, uppercase sql

--- db.sql
+++ db.sql
@@ -11,4 +11,4 @@ CREATE TABLE user (
                       updated_at DATETIME
 );
 
-INSERT INTO user (email, password, created_at) VALUES ('testowy@gmail.com', '$2y$13$2fF1fTH6Psy/9ToPypADF.FrVdATVUrx/HHiBS7Uj4YVp0t7/E0Vy', NOW());
+INSERT INTO user (email, password, roles, created_at) VALUES ('testowy@gmail.com', '$2y$13$2fF1fTH6Psy/9ToPypADF.FrVdATVUrx/HHiBS7Uj4YVp0t7/E0Vy', JSON_ARRAY('ROLE_USER'), NOW());
